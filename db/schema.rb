8a89237db2d5b167449cf54f65cc24615c7c992a
philmphoenix <phillip.matthew.phoenix@gmail.com>
2015-02-22 15:31:06 -0700

    Through uuid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222202935) do
+ActiveRecord::Schema.define(version: 20150222222723) do
 
   create_table "purchases", force: :cascade do |t|
     t.string   "email"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150222202935) do
     t.integer  "product_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "uuid"
   end
 
 end
