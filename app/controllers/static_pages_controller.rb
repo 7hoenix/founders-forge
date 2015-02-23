2d09c881334925c1976143e35a83ce354a8023ea
philmphoenix <phillip.matthew.phoenix@gmail.com>
2015-02-22 17:56:04 -0700

    Reworked views to use product information

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
 
   def main
+    @product = Product.find_by_sku("FORGE1")
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-founder_forge = Product.create( title: "Founders Forge",
+forge_brad_feld = Product.create( title: "Founders Forge",
                                 subtitle: "Continued Education for Founders",
                                 author: "Justin Holzmann",
                                 price: "4.99",
                                 sku: "FORGE1",
+                                length: "30 minutes",
+                                author_image_name: "teacher-image.jpg",
+                                details: "You'll get one video",
+                                download_url: "https://s3.amazonaws.com/founders-forge/Practice+Like+a+Pro+1.0.pdf",
                                 description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
 
                                 <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
@@ -20,4 +24,8 @@ founder_forge = Product.create( title: "Founders Forge",
                                     <li>The 5 stages of the user lifecycle</li>
                                     <li>How to apply the Lean Marketing Framework</li>
                                     <li>Resources and tools you'll need to know</li>
-                                </ul>})
+                                </ul>},
+                                author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.
+                                </p>
+                                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
